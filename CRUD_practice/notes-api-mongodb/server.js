7cd998ebfafff615f243af59939565bbb0bd4f18
Mayaz120102 <abrarmayaz2002@gmail.com>
2026-05-17 00:15:36 +0600

    cors update and cookie parser adding

--- CRUD_practice/notes-api-mongodb/server.js
+++ CRUD_practice/notes-api-mongodb/server.js
@@ -3,14 +3,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 dotenv.config(); //laod env
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
+//body parser
 app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
 
 //connect database
 connectDB();
